7cb36b7ae47c869ccfccb5a2d2b2c80dd33640ab
1p22geo <1p22geodecki@gmail.com>
2024-09-04 16:46:57 +0200

    feat: added CopyQ support

--- passgen.py
+++ passgen.py
@@ -1,20 +1,38 @@
 import random
 import string
 import pyperclip
+import subprocess
+
 
 def generate_password(length=20):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
+
+def run_command_stdin(command: list[str], stdin: str):
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE,
+                            stdin=subprocess.PIPE, stderr=subprocess.STDOUT)
+    proc.communicate(input=stdin.encode())
+
+
+def copy_password(password):
+    """
+    CROSS-PLATFORM function to copy a string into clipboard.
+    """
+
+    # Pyperclip (working on Windows)
+    pyperclip.copy(password)
+
+    # CopyQ (working on Linux)
+    run_command_stdin(["copyq", "copy", "-"], password)
+
+
 if __name__ == "__main__":
     generated_password = generate_password()
-    
-    # Copy the generated password to clipboard
-    pyperclip.copy(generated_password)
-    
-    # Print the generated password
+
     print("Generated Password:", generated_password)
-    
-    # Notify the user
+
+    copy_password(generated_password)
+
     print("Password copied to clipboard.")
